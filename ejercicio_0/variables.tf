149ea2591c362c9053e95d0aa3c1bf6060105d92
Elena Morales <elenamoralesp7@gmail.com>
2026-03-07 09:01:00 +0100

    Commit

--- ejercicio_0/variables.tf
+++ ejercicio_0/variables.tf
@@ -1,19 +1,30 @@
 variable "filename" {
     default = "pets.txt"
+    type = string
 }
 
 variable "content"{
     default = "Me gusta la tortilla"
+    type = string
 }
 
 variable "prefix" {
     default = "Mrs"
+    type = string
 }
 
 variable "separator" {
     default = "."
+    type = string
 }
 
 variable "length" {
     default = "7"
+    type = string
+}
+
+
+variable "password_change" {
+    default = "true"
+    type = bool
 }
